942def6162ebed29b42c35bd0e8f7a751a173f15
wlzerd <64091355+wlzerd@users.noreply.github.com>
2025-07-13 22:13:02 +0900

    Add Docker support and update database path

--- database.js
+++ database.js
@@ -1,5 +1,14 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./data.db');
+const fs = require('fs');
+const path = require('path');
+
+const dataDir = path.join(__dirname, 'data');
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir);
+}
+
+const dbPath = path.join(dataDir, 'data.db');
+const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS members (
